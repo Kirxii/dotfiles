bef07d57870914382d2014ec04405eb3686cf8eb
Kirxii <131458787+Kirxii@users.noreply.github.com>
2026-09-13 16:38:09 +0700

    fix: Complete experience of surrounding with delimeter and endwise

--- .config/nvim/lua/plugins/text-editing/delimiters.lua
+++ .config/nvim/lua/plugins/text-editing/delimiters.lua
@@ -6,15 +6,10 @@ return {
 		opts = {
 			map_bs = true,
 		},
-		config = function(_, opts)
-			local npairs = require("nvim-autopairs")
-			npairs.setup(opts)
-
-			-- endwise combatibility
-			npairs.add_rules(require("nvim-autopairs.rules.endwise-elixir"))
-			npairs.add_rules(require("nvim-autopairs.rules.endwise-lua"))
-			npairs.add_rules(require("nvim-autopairs.rules.endwise-ruby"))
-		end,
+	},
+	{
+		"fell-z/nvim-treesitter-endwise",
+		event = { "BufReadPre", "BufNewFile" },
 	},
 	{
 		"windwp/nvim-ts-autotag",
@@ -23,7 +18,45 @@ return {
 		opts = {},
 	},
 	{
+		"kylechui/nvim-surround",
+		version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = { "BufReadPre", "BufNewFile" },
+
+		opts = {},
+	},
+	{
 		"HiPhish/rainbow-delimiters.nvim",
 		event = { "BufReadPre", "BufNewFile" },
+	},
+	{
+		-- Allows tab to exit out of parenthesises
+		"abecodes/tabout.nvim",
+		event = { "InsertEnter" },
+
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+
+		opts = {
+			tabkey = "<Tab>",
+			backwards_tabkey = "<S-Tab>",
+			act_as_tab = true,
+			act_as_shift_tab = true,
+			default_tab = "<C-t>",
+			default_shift_tab = "<C-d>",
+			enable_backwards = true,
+			completion = true,
+			tabouts = {
+				{ open = "'", close = "'" },
+				{ open = '"', close = '"' },
+				{ open = "`", close = "`" },
+				{ open = "(", close = ")" },
+				{ open = "[", close = "]" },
+				{ open = "{", close = "}" },
+				{ open = "<", close = ">" },
+			},
+			ignore_beginning = false,
+			exclude = {},
+		},
 	},
 }
